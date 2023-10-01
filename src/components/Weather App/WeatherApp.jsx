17ca416f8c7c99e062828c0630126297e5182b5c
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-01 17:35:39 +0300

    weather icon functionality

--- src/components/Weather App/WeatherApp.jsx
+++ src/components/Weather App/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 import search_icon from "../Assets/search.png"
 import clear_icon from "../Assets/clear.png"
@@ -12,10 +12,11 @@ import humidity_icon from "../Assets/humidity.png"
 
 const WeatherApp = () => {
     const api_key = "8af57fa75f00d26247977592083a4100"
+    const [wicon, setWicon] = useState(cloud_icon);
 
     const search = async() => {
         // getting the input from the search bar
-        const element = document.getElementByClassName('cityInput')
+        const element = document.getElementsByClassName('cityInput')
         let city = element[0].value
         if (city === ""){
             return 0
@@ -25,19 +26,21 @@ const WeatherApp = () => {
         // element[0] because there is only one element with this class
         // if we were searching by ID there wouldn't be any need of indexing [0]
         
-        let response = await fetch(url)
-        let data = await response.json()
-        const humidity = document.getElementByClassName('humidity-percentage')
-        const wind = document.getElementByClassName('wind-rate')
-        const temperature = document.getElementsByClassName('weather-temperature')
-        const location = document.getElementsByClassName('weather-location')
+        let response = await fetch(url);
+        let data = await response.json();
+        const humidity = document.getElementsByClassName('humidity-percentage');
+        const wind = document.getElementsByClassName('wind-rate');
+        const temperature = document.getElementsByClassName('weather-temperature');
+        const location = document.getElementsByClassName('weather-location');
     
-        humidity[0].innerHTML = data.main.humidity;
-        wind[0].innerHTML = data.wind.speed;
-        temperature[0].innerHTML = data.main.temp;
-        location[0].innerHTML = data.name
+        humidity[0].innerHTML = data.main.humidity + "%";
+        wind[0].innerHTML = data.wind.speed + "km/h";
+        temperature[0].innerHTML = data.main.temp.toFixed(0) + "°C";
+        location[0].innerHTML = data.name;
+        console.log(city);
     
-    
+        if (data.weather[0].icon===)
+        // using weather[0] because the weather holds an array and inside it there is only one object so we have to use index to access it
     }
   return (
     <div className='container'>
