db84b8967519311357aa2bb93c56195153f9806b
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-01 16:16:41 +0300

    jsx

--- src/components/Weather App/WeatherApp.jsx
+++ src/components/Weather App/WeatherApp.jsx
@@ -19,7 +19,27 @@ const WeatherApp = () => {
                 <img src={search_icon} alt="" />
             </div>
         </div>
-
+        <div className="weather-image">
+            <img src={cloud_icon} alt="" />
+        </div>
+        <div className="weather-temperature">24°C</div>
+        <div className="weather-location">London</div>
+        <div className="data-container">
+            <div className="element">
+                <img src={humidity_icon} alt="" className="icon" />
+                <div className="data">
+                    <div className="humidity-percentage">64%</div>
+                    <div className="text">Humidity</div>
+                </div>
+            </div>
+            <div className="element">
+                <img src={wind_icon} alt="" className="icon" />
+                <div className="data">
+                    <div className="wind-speed">18km/h</div>
+                    <div className="text">Wind speed</div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
